26c267fad6b2f43e535eb9ed4be16ad3d675c530
carlosgaxiola <carlosgaxiola@solemti.com>
2022-05-16 13:28:00 -0600

    Scroll controls para mas vendidos

--- src/components/body/MasVendidos.js
+++ src/components/body/MasVendidos.js
@@ -1,19 +1,38 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import Product from './Product';
 import './MasVendidos.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faAngleRight, faAngleLeft } from '@fortawesome/free-solid-svg-icons'
 
 const MasVendidos = () => {
   const [products, setProducts] = useState([]);
-
+  const list = useRef();
+  
   useEffect( () => {
     getProducts();
   }, []);
 
-  let getProducts = async() => {
+  const getProducts = async() => {
     const urlProducts = 'https://corebiz-test.herokuapp.com/api/v1/products';
     const resp = await fetch(urlProducts);
     const data = await resp.json();
     setProducts(data);
+  }
+
+  const scrollLeft = () => {
+    list.current.scrollTo({
+      top: 0,
+      left: -315,
+      behavior: 'smooth'
+    });
+  }
+
+  const scrollRight = () => {
+    list.current.scrollTo({
+      top: 0,
+      left: 315,
+      behavior: 'smooth'
+    });
   }
 
   return (
@@ -21,7 +40,7 @@ const MasVendidos = () => {
       <div className="mas-vendidos">
         <h2 className="productos-title">Más Vendidos</h2>
         <div className="separator"></div>
-        <ul className="product-list">
+        <ul ref={ list } className="product-list">
           { 
             products.map( product => (
               <Product key={product.productId}
@@ -29,6 +48,12 @@ const MasVendidos = () => {
             ))
           }
         </ul>
+        <div onClick={ scrollLeft } className="scroll-control scroll-left">
+          <FontAwesomeIcon className="scroll-control-icon" icon={faAngleLeft} />
+        </div>
+        <div onClick={ scrollRight } className="scroll-control scroll-right">
+          <FontAwesomeIcon className="scroll-control-icon" icon={faAngleRight} />
+        </div>
       </div>
     </>
   );
